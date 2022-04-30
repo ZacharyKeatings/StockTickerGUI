37b90956076caf5c79cc20bcebe73e7e0cc4ca84
Zachary Keatings <zachary.keatings@gmail.com>
2022-04-29 22:23:57 -0400

    Added non-functioning New Game frame

--- main.py
+++ main.py
@@ -17,13 +17,21 @@ class MainWindow(tk.Tk):
         self.clear()
         MainMenu(parent = self).pack(fill="both", expand="true")
 
+    def switch_to_new_game(self):
+        self.clear()
+        NewGame(parent = self).pack(fill="both", expand="true") 
+
     def switch_to_about_page(self):
         self.clear()
         AboutPage(parent = self).pack(fill="both", expand="true")
 
-    # def switch_to_start_game(self):
-    #     self.clear()
-    #     AboutPage(parent = self).pack(fill="both", expand="true")
+    def switch_to_load_game(self):
+        self.clear()
+        AboutPage(parent = self).pack(fill="both", expand="true") ######### 
+
+    def switch_to_highscores(self):
+        self.clear()
+        AboutPage(parent = self).pack(fill="both", expand="true") #########
 
     def clear(self):
         for widget in self.winfo_children():
@@ -45,37 +53,73 @@ class MainMenu(tk.Frame):
         ).grid(row=0, column=0, columnspan=2, sticky="new")
         tk.Button(
             master = self, 
-            text="About",
-            command = self.parent.switch_to_about_page
+            text="New Game",
+            command = self.parent.switch_to_new_game
         ).grid(row=1, column=0, columnspan=2, sticky="sew")
         tk.Button(
             master = self, 
-            text="Load Game",
-            command = self.parent.switch_to_about_page, ############
-            state = tk.DISABLED
+            text="About",
+            command = self.parent.switch_to_about_page
         ).grid(row=2, column=0, columnspan=2, sticky="sew")
         tk.Button(
             master = self, 
-            text="Highscores",
-            command = self.parent.switch_to_about_page, ############
+            text="Load Game",
+            command = self.parent.switch_to_load_game, 
             state = tk.DISABLED
         ).grid(row=3, column=0, columnspan=2, sticky="sew")
         tk.Button(
             master = self, 
+            text="Highscores",
+            command = self.parent.switch_to_highscores, 
+            state = tk.DISABLED
+        ).grid(row=4, column=0, columnspan=2, sticky="sew")
+        tk.Button(
+            master = self, 
             text="Quit", 
             command=lambda : exit()
-        ).grid(row=4, column=0, columnspan=2, sticky='sew')
+        ).grid(row=5, column=0, columnspan=2, sticky='sew')
+
+class NewGame(tk.Frame):
+ 
+    def __init__(self, parent: MainWindow):
+        tk.Frame.__init__(self, master = parent, bg="green")
+        self.parent = parent
+        self.grid_columnconfigure(0, weight=1)
+        self.grid_rowconfigure(1, weight=1)
+
+        tk.Label(
+            master = self, 
+            text="New game", 
+            bg="green" 
+        ).grid(row=0, column=0, sticky='new')
+        tk.Label(
+            master = self, 
+            text="Please choose the number of players:", 
+            bg="green"
+        ).grid(row=1, column=0, sticky='new')
+        tk.Label(
+            master = self, 
+            text="test text",
+            font=("Arial", 16) 
+        ).grid(row=1, column=1, sticky='new')
+        tk.Button(
+            master = self, 
+            text="Main Menu", 
+            command = self.parent.switch_to_main_menu
+        ).grid(row=2, column=0, columnspan=2, sticky="sew")
+        tk.Button(
+            master = self, 
+            text="Quit", 
+            command=lambda : exit()
+        ).grid(row=3, column=0, columnspan=2, sticky="sew")
 
 class AboutPage(tk.Frame):
  
     def __init__(self, parent: MainWindow):
         tk.Frame.__init__(self, master = parent, bg="green")
         self.parent = parent
-        self.grid_rowconfigure(0, weight=0)
         self.grid_columnconfigure(0, weight=1)
         self.grid_rowconfigure(1, weight=1)
-        self.grid_rowconfigure(2, weight=0)
-        self.grid_rowconfigure(3, weight=0)
 
         tk.Label(
             master = self, 
