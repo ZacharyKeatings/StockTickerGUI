bfdc4df35c0335253acbc7f1673b7b6fe0532bde
Zachary Keatings <zachary.keatings@gmail.com>
2022-05-09 22:42:32 -0400

    Added Game setup page: click pass changes turn to next player

--- README.md
+++ README.md
@@ -3,11 +3,11 @@ A graphical version of Stock Ticker
 
 ## Stock Ticker
 
-The object of the game is to buy and sell stocks, and by so doing accumulate a greater amount of money than the other players. The winner is decided by setting a time limit at the start of the game, and is the person having the greatest amount of money when time elapses, after selling his stocks back to the Broker at their final market value.
+The object of the game is to buy and sell stocks, and by so doing accumulate a greater amount of money than the other self. The winner is decided by setting a time limit at the start of the game, and is the person having the greatest amount of money when time elapses, after selling his stocks back to the Broker at their final market value.
 
 #### Features:
 
-* Can choose number of players and number of rounds.
+* Can choose number of self and number of rounds.
 * Players can be given custom names.
 * About page with short description of game.
 * The 'Quit' button closes the program.
@@ -24,7 +24,7 @@ This is a GUI version of the console-based Stock Ticker which can be found in th
 * Add option to save game
 * Add option to load game
 * Add highscore list organized by number of rounds played
-* Add networking option to allow players to play remotely together
+* Add networking option to allow self to play remotely together
 
 #### Inspiration:
 
--- main.py
+++ main.py
@@ -13,12 +13,13 @@ class MainWindow(tk.Tk):
         self.mainmenu = MainMenu(parent=self)
         self.current = self.mainmenu
 
-        self.mainmenu.pack(fill="both", expand="true")
+        self.mainmenu.pack(fill="both", expand=1)
 
     def switch_to(self, target):
         self.current.pack_forget()
         self.current = target
-        self.current.pack(fill="both", expand="true")
+        self.current.pack(fill="both", expand=1)
+
 
 class MainMenu(tk.Frame):
 
@@ -28,9 +29,11 @@ class MainMenu(tk.Frame):
         self.grid_rowconfigure(0, weight=1)
         self.grid_columnconfigure(0, weight=1)
 
+        #!Testing purposes only. Remove after completion===
         def list_players():
             for i in range(len(Player.players)):
                 print(Player.players[i].name)
+        #!=================================================
 
         tk.Label(
             master=self,
@@ -71,6 +74,7 @@ class MainMenu(tk.Frame):
             command=list_players
         ).grid(row=6, column=0, columnspan=2, sticky='sew')
 
+
 class AboutPage(tk.Frame):
 
     def __init__(self, parent: MainWindow):
@@ -102,6 +106,7 @@ class AboutPage(tk.Frame):
             command=exit
         ).grid(row=3, column=0, columnspan=2, sticky="sew")
 
+
 class NewGame(tk.Frame):
 
     def __init__(self, parent: MainWindow):
@@ -114,8 +119,8 @@ class NewGame(tk.Frame):
         self.grid_columnconfigure(1, weight=1)
         self.grid_columnconfigure(2, weight=1)
 
-        self.player_range = [i for i in range(2,9)]
-        self.round_range = [i for i in range(1,101)]
+        self.player_range = [i for i in range(2, 9)]
+        self.round_range = [i for i in range(1, 101)]
 
         self.num_players = tk.IntVar()
         self.num_rounds = tk.IntVar()
@@ -130,27 +135,32 @@ class NewGame(tk.Frame):
             text="Please choose the number of players:",
             bg="green"
         ).grid(row=1, column=1, sticky='n')
-        ttk.Combobox(
+        players = ttk.Combobox(
             master=self,
             textvariable=self.num_players,
             state="readonly",
             values=self.player_range
-        ).grid(row=2, column=1, sticky='n')
+        )
+        players.grid(row=2, column=1, sticky='n')
+        players.current(0)
         tk.Label(
             master=self,
             text="Please choose the number of rounds:",
             bg="green"
         ).grid(row=3, column=1, sticky='n')
-        ttk.Combobox(
+        rounds = ttk.Combobox(
             master=self,
             textvariable=self.num_rounds,
             state="readonly",
             values=self.round_range
-        ).grid(row=4, column=1, sticky='n')
+        )
+        rounds.grid(row=4, column=1, sticky='n')
+        rounds.current(0)
         ttk.Button(
             master=self,
             text="Submit",
-            command=lambda: [Game.set_players(self.num_players.get()), Game.set_rounds(self.num_rounds.get()), self.parent.switch_to(target=PlayerNamePage(parent=self.parent))]
+            command=lambda: [Game.set_players(self.num_players.get()), Game.set_rounds(self.num_rounds.get()),
+                             self.parent.switch_to(target=PlayerNamePage(parent=self.parent))]
         ).grid(row=5, column=1, sticky="new")
         ttk.Button(
             master=self,
@@ -162,6 +172,7 @@ class NewGame(tk.Frame):
             text="Quit",
             command=exit
         ).grid(row=7, column=0, columnspan=3, sticky="sew")
+
 
 class PlayerNamePage(tk.Frame):
 
@@ -183,7 +194,8 @@ class PlayerNamePage(tk.Frame):
         self.p7_name = tk.StringVar()
         self.p8_name = tk.StringVar()
 
-        self.name_list = [self.p1_name, self.p2_name, self.p3_name, self.p4_name, self.p5_name, self.p6_name, self.p7_name, self.p8_name]
+        self.name_list = [self.p1_name, self.p2_name, self.p3_name, self.p4_name, 
+                          self.p5_name, self.p6_name, self.p7_name, self.p8_name]
 
         def set_names():
             Player.players.clear()
@@ -191,12 +203,14 @@ class PlayerNamePage(tk.Frame):
                 player = Player(name=self.name_list[player_name].get())
                 Player.players.append(player)
 
-        #create all 8 player widgets, but only pack based on num_players
+        # create all 8 player widgets, but only pack based on num_players
         for num in range(Game.num_players):
             self.grid_rowconfigure(num + 1, weight=1)
             tk.Label(master=self, text=f"Player {num + 1}:", bg="green").grid(row=num + 1, column=1, sticky="w")
             tk.Entry(master=self, textvariable=self.name_list[num]).grid(row=num + 1, column=1, sticky="e")
-            ttk.Button(master=self, text="Submit", command=lambda : [set_names(), self.parent.switch_to(target=GameSetup(parent=self.parent))]).grid(row=9, column=1, sticky="new")
+            ttk.Button(master=self, text="Submit",
+                       command=lambda: [set_names(), self.parent.switch_to(target=GameSetup(parent=self.parent))]
+                      ).grid(row=9, column=1, sticky="new")
 
         tk.Label(
             master=self,
@@ -215,13 +229,25 @@ class PlayerNamePage(tk.Frame):
             command=exit
         ).grid(row=10, column=0, columnspan=3, sticky="sew")
 
+
+#!create labelframe for [Buy, Sell, Pass] buttons.
+#!create labelframes for each player that displays there money and all stock quantities.
+#!display current players turn
+#!buy button brings new frame on top of buy/sell/pass frame with 7 buttons: the 6 stocks plus back button.
+#!after choosing stock,new labelframe goes on top with combobox and 2 buttons [ok, back].
+#!combobox displays number of possible amounts to buy from 1 to max amount possible based on current player money.
+#!after choosing stock amount and pressing ok, last frame appears with button labelled [are you sure?]
 class GameSetup(tk.Frame):
 
     def __init__(self, parent: MainWindow):
         tk.Frame.__init__(self, master=parent, bg="green")
         self.parent = parent
-        self.grid_rowconfigure(0, weight=1)
-        self.grid_rowconfigure(9, weight=1)
+        self.grid_rowconfigure(1, weight=1)
+        self.grid_rowconfigure(2, weight=1)
+        self.grid_rowconfigure(3, weight=1)
+        self.grid_rowconfigure(4, weight=1)
+        self.grid_rowconfigure(5, weight=1)
+        self.grid_rowconfigure(6, weight=1)
         self.grid_columnconfigure(0, weight=1)
         self.grid_columnconfigure(1, weight=1)
         self.grid_columnconfigure(2, weight=1)
@@ -230,20 +256,120 @@ class GameSetup(tk.Frame):
             master=self,
             text="Game Setup round",
             bg="green"
-        ).grid(row=0, column=1, sticky='new')
+        ).grid(row=0, column=0, columnspan=3, sticky='new')
+        cur_player = tk.LabelFrame(
+            master=self,
+            text="Current Player:",
+            bg="green"
+        )
+        cur_player.grid(row=1, column=1, sticky="ew")
+
+        tk.Label(
+            master=cur_player,
+            text=Game.current_player_name(),
+            bg="green"
+        ).grid(column=1)
+
+        self.player_grid = []
+        for num in range(Game.num_players):
+            widget = tk.LabelFrame(
+                master=self,
+                text=f"{Player.players[num].name}:",
+                bg="green"
+            )
+            self.player_grid.append(widget)
+
+        self.player_grid[0].grid(row=3, column=0, sticky="nsew")
+        self.player_grid[1].grid(row=3, column=2, sticky='nsew')
+        if Game.num_players == 3:
+            self.player_grid[2].grid(row=4, column=1, sticky='nsew')
+        if Game.num_players == 4:
+            self.player_grid[2].grid(row=4, column=1, sticky='nsew')
+            self.player_grid[3].grid(row=2, column=1, sticky='nsew')
+        if Game.num_players == 5:
+            self.player_grid[2].grid(row=4, column=1, sticky='nsew')
+            self.player_grid[3].grid(row=2, column=1, sticky='nsew')
+            self.player_grid[4].grid(row=2, column=0, sticky='nsew')
+        if Game.num_players == 6:
+            self.player_grid[2].grid(row=4, column=1, sticky='nsew')
+            self.player_grid[3].grid(row=2, column=1, sticky='nsew')
+            self.player_grid[4].grid(row=2, column=0, sticky='nsew')
+            self.player_grid[5].grid(row=2, column=2, sticky='nsew')
+        if Game.num_players == 7:
+            self.player_grid[2].grid(row=4, column=1, sticky='nsew')
+            self.player_grid[3].grid(row=2, column=1, sticky='nsew')
+            self.player_grid[4].grid(row=2, column=0, sticky='nsew')
+            self.player_grid[5].grid(row=2, column=2, sticky='nsew')
+            self.player_grid[6].grid(row=4, column=0, sticky='nsew')
+        if Game.num_players == 8:
+            self.player_grid[2].grid(row=4, column=1, sticky='nsew')
+            self.player_grid[3].grid(row=2, column=1, sticky='nsew')
+            self.player_grid[4].grid(row=2, column=0, sticky='nsew')
+            self.player_grid[5].grid(row=2, column=2, sticky='nsew')
+            self.player_grid[6].grid(row=4, column=0, sticky='nsew')
+            self.player_grid[7].grid(row=4, column=2, sticky='nsew')
+        
+        for num in range(Game.num_players):
+            tk.Label(
+                master=self.player_grid[num],
+                text=f"Money: {Player.players[num].money}",
+                bg="green"
+            ).grid(sticky='w')
+            tk.Label(
+                master=self.player_grid[num],
+                text=f"Gold: {Player.players[num].stocks['Gold']}",
+                bg="green"
+            ).grid(sticky='w')
+            tk.Label(
+                master=self.player_grid[num],
+                text=f"Silver: {Player.players[num].stocks['Silver']}",
+                bg="green"
+            ).grid(sticky='w')
+            tk.Label(
+                master=self.player_grid[num],
+                text=f"Oil: {Player.players[num].stocks['Oil']}",
+                bg="green"
+            ).grid(sticky='w')
+            tk.Label(
+                master=self.player_grid[num],
+                text=f"Bonds: {Player.players[num].stocks['Bonds']}",
+                bg="green"
+            ).grid(sticky='w')
+            tk.Label(
+                master=self.player_grid[num],
+                text=f"Grain: {Player.players[num].stocks['Grain']}",
+                bg="green"
+            ).grid(sticky='w')
+            tk.Label(
+                master=self.player_grid[num],
+                text=f"Industrial: {Player.players[num].stocks['Industrial']}",
+                bg="green"
+            ).grid(sticky='w')
+
+        ttk.Button(
+            master=self,
+            text="Pass",
+            command=lambda: [Game.next_player(), self.parent.switch_to(target=GameSetup(parent=self.parent))]
+        ).grid(row=3, column=1)
 
         ttk.Button(
             master=self,
             text="Back",
             command=lambda: self.parent.switch_to(target=PlayerNamePage(parent=self.parent))
-        ).grid(row=9, column=0, columnspan=3, sticky="sew")
+        ).grid(row=6, column=0, columnspan=3, sticky="sew")
+        ttk.Button(
+            master=self,
+            text="Main Menu",
+            command=lambda: self.parent.switch_to(target=MainMenu(parent=self.parent))
+        ).grid(row=7, column=0, columnspan=3, sticky="sew")
         ttk.Button(
             master=self,
             text="Quit",
             command=exit
-        ).grid(row=10, column=0, columnspan=3, sticky="sew")
+        ).grid(row=8, column=0, columnspan=3, sticky="sew")
 
-class Player():
+
+class Player:
     players = []
 
     def __init__(self, name):
@@ -258,17 +384,18 @@ class Player():
             "Industrial": 0
         }
 
-    def create_player(player_name):
-        new_player = Player(player_name)
+    def create_player():
+        new_player = Player()
         Player.players.append(new_player)
 
-    def buy_stock(stock):
+    def buy_stock(self):
         pass
 
-    def sell_stock(stock):
+    def sell_stock(self):
         pass
 
-class Stock():
+
+class Stock:
     stock_value = {
         "Gold": 100,
         "Silver": 100,
@@ -278,21 +405,24 @@ class Stock():
         "Industrial": 100
     }
 
-    def increase_value(stock):
+    def increase_value(self):
         pass
 
-    def decrease_value(stock):
+    def decrease_value(self):
         pass
 
-    def double_stock(stock):
+    def double_stock(self):
         pass
 
-    def split_stock(stock):
+    def split_stock(self):
         pass
 
-class Game():
+
+class Game:
     num_players = 0
+    curr_player = 0
     max_rounds = 0
+    curr_round = 0
 
     def set_players(players):
         Game.num_players = players
@@ -300,6 +430,14 @@ class Game():
     def set_rounds(rounds):
         Game.max_rounds = rounds
 
+    def current_player_name():
+        return Player.players[Game.curr_player].name
+
+    def next_player():
+        if Game.curr_player == Game.num_players-1:
+            Game.curr_player = 0
+        else:
+            Game.curr_player += 1
 
 def main():
     return MainWindow().mainloop()
